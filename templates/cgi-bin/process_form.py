ab3203a02f1126cfc9a297febff529ffeaa15e5f
Aditya Choudhary <adi854105@gmail.com>
2023-07-29 17:53:19 +0530

    first commit

---
+++ templates/cgi-bin/process_form.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import cgi
+
+# Set the content type to HTML
+print("Content-type: text/html\n")
+
+# Retrieve form data
+form = cgi.FieldStorage()
+post_topic = form.getvalue("post_topic")
+target = form.getvalue("target")
+num_of_days = form.getvalue("num_of_days")
+target_audience = form.getvalue("target_audience")
+
+# You can process the form data further as per your requirements
+# For now, we'll just print the values for demonstration purposes
+print("<h1>LinkedIn Post Automator - Form Submission</h1>")
+print("<p>Post Topic:", post_topic, "</p>")
+print("<p>Target:", target, "</p>")
+print("<p>Number of Days:", num_of_days, "</p>")
+print("<p>Target Audience Type:", target_audience, "</p>")
