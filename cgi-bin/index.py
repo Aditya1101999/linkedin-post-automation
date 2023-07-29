65a8cf658de4817ad6061b891571ff1b0ebf308c
Aditya Choudhary <adi854105@gmail.com>
2023-07-29 22:58:37 +0530

    images fixed'

--- cgi-bin/index.py
+++ cgi-bin/index.py
@@ -5,16 +5,15 @@ import requests
 from PIL import Image, ImageDraw, ImageFont
 import time
 
-# Set the content type to HTML
 print("Content-type: text/html\n")
 
-# todo(right one) OpenAI API key
-openai.api_key = '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'
+openai.api_key = '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'
 
 form = cgi.FieldStorage()
 post_topic = form.getvalue("post_topic")
 target = form.getvalue("target")
 num_of_days = form.getvalue("num_of_days")
+num_of_days = int(num_of_days) if num_of_days is not None else 1
 target_audience = form.getvalue("target_audience")
 
 # adjust prompt
@@ -29,7 +28,11 @@ def generate_image(post_heading):
     canvas = Image.new('RGB', (canvas_width, canvas_height), rectangle_color)
     draw = ImageDraw.Draw(canvas)
 
-    font = ImageFont.truetype('arial.ttf', size=30)
+    try:
+        font = ImageFont.truetype('arial.ttf', size=30)
+    except IOError:
+        font = ImageFont.load_default()
+
     text_width, text_height = draw.textsize(post_heading, font=font)
     text_x = (canvas_width - text_width) // 2
     text_y = (canvas_height - text_height) // 2
@@ -39,30 +42,28 @@ def generate_image(post_heading):
     return canvas
 
 for day in range(1, num_of_days + 1):
-    response = openai.Completion.create(
-        engine="gpt-3.5-turbo",
-        prompt=prompt,
-        max_tokens=600,
+    response = openai.ChatCompletion.create(  
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": prompt},
+        ],
     )
 
-    generated_content = response['choices'][0]['text']
+    generated_content = response['choices'][0]['message']['content']
 
-    # Generate image for the post
     post_heading = generated_content
     image = generate_image(post_heading)
 
-    # Save the image with a unique filename for each day
     output_directory = '/Users/adi/Developer/DevProjects/Python/linkedin-postautomater/generated_images'
     image_path = f"{output_directory}/image_day{day}.png"
     image.save(image_path)
 
-    # Display a confirmation message to the user
     print("<h1>LinkedIn Post Automator - Content Generated</h1>")
     print("<p>Generated Content for Day:", day, ":</p>")
     print("<p>", generated_content, "</p>")
     print("<p>Generated Image for Day:", day, ":</p>")
     print(f'<img src="{image_path}" alt="Generated Image" width="400">')
 
-    # Introduce a 24-hour gap between posts
     if day < num_of_days:
-        time.sleep(24 * 60 * 60)  
+        time.sleep(24 * 60 * 60)
