4695df23cff449a618d4ebe6261295d7e4c70793
Ayush Kumar <ayush.kumar.21cse@bmu.edu.in>
2025-02-26 19:41:39 +0530

    UI fixes

--- client/src/components/Preview.js
+++ client/src/components/Preview.js
@@ -1,12 +1,31 @@
 import Markdown from "react-markdown"
-export default function Preview({content, image, days}){
+export default function Preview({ content, image, days }) {
     return (
         <>
-        <Markdown className="mt-4 bg-zinc-100 border border-zinc-300 rounded-xl p-6">
-                            {content}
-                        </Markdown>
-        <img>{image}</img>
+            {/* <img>{image}</img>
         <h3>{days}</h3>
+         */}
+            <h1 className="max-width text-md font-montserrat font-semibold">Preview And Automate</h1>
+            <div className="max-width m-auto bg-gray-100 mt-5 flex items-center justify-center">
+                <div className="bg-white border shadow-sm px-4 py-3 rounded-lg w-full">
+                    <div className="flex items-center">
+                        <div className="h-12 w-12 rounded-full bg-gray-200" />
+                        <div className="ml-2">
+                            <div className="text-sm font-montserrat">
+                                <span className="font-semibold">John Doe</span>
+                                <span className="text-gray-500"> • 1st</span>
+                            </div>
+                            <div className="text-gray-500 text-xs font-montserrat">Software Engineer at Tesla, Inc</div>
+                            <div className="text-gray-500 text-xs font-montserrat flex">
+                                <span className="inline-block">3d • Edited</span>
+                            </div>
+                        </div>
+                    </div>
+                    <Markdown className="mt-4 p-2 font-montserrat">
+                        {content ? content : ("Your Generated Content goes here")}
+                    </Markdown>
+                </div>
+            </div>
         </>
     )
 }
